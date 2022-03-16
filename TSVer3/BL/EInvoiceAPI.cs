a11640c026cadc142c141993a5cd097bb0712b12
dhaneshkumar-s <79852490+dhaneshkumar-s@users.noreply.github.com>
2022-03-16 09:55:52 +0530

    added restclient api call

--- TSVer3/BL/EInvoiceAPI.cs
+++ TSVer3/BL/EInvoiceAPI.cs
@@ -16,16 +16,22 @@ namespace TSVer3.BL
         public AuthTokenModel GetAuthToken()
         {
             AuthTokenModel authTokenModel = new AuthTokenModel();
+            
+            var client = new RestClient("https://gstsandbox.charteredinfo.com/eivital/dec/v1.04/auth?&aspid=1677089922&password=Etrack@630&Gstin=34AACCC1596Q002&user_name=TaxProEnvPON&eInvPwd=abc34*");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            authTokenModel = JsonConvert.DeserializeObject<AuthTokenModel>(response.Content);
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://gstsandbox.charteredinfo.com/eivital/dec/v1.04/auth?&aspid=1677089922&password=Etrack@360&Gstin=34AACCC1596Q002&user_name=TaxProEnvPON&eInvPwd=abc34*");
-            request.Method = "GET";
-            request.ContentType = "application/json";
-            WebResponse response = request.GetResponse();
-            using (var reader = new StreamReader(response.GetResponseStream()))
-            {
-                var ApiStatus = reader.ReadToEnd();
-                authTokenModel = JsonConvert.DeserializeObject<AuthTokenModel>(ApiStatus);
-            }  
+           // HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://gstsandbox.charteredinfo.com/eivital/dec/v1.04/auth?&aspid=1677089922&password=Etrack@360&Gstin=34AACCC1596Q002&user_name=TaxProEnvPON&eInvPwd=abc34*");
+            //request.Method = "GET";
+            //request.ContentType = "application/json";
+            //WebResponse response = request.GetResponse();
+            //using (var reader = new StreamReader(response.GetResponseStream()))
+           // {
+            //    var ApiStatus = reader.ReadToEnd();
+            //    authTokenModel = JsonConvert.DeserializeObject<AuthTokenModel>(ApiStatus);
+            //}  
             return authTokenModel;
         }
     }
